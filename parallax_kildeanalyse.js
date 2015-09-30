3dd5c6d4e11d8b29532ef7034c40ca9dfcc0dfae
hallandsen <jacob.e.halland@gmail.com>
2015-10-01 00:00:46 +0200

    no message

--- parallax_kildeanalyse.js
+++ parallax_kildeanalyse.js
@@ -1,32 +1,36 @@
-// ScrollMagic vars
-
+$(document).ready(function() {
 // init controller
-var controller = new ScrollMagic.Controller();
+	var controller = new ScrollMagic.Controller();
 
-//create a scene
-var scene = new ScrollMagic.Scene({
-	triggerElement: '#scene-3'})//farven skifter når scene-3 rammer midten af viewport
-		.on('enter', function(){
-			animateElement.style.fill = 'grey';
-		});
-		// .on('leave', function(){
-		// 	animateElement.style.fill = "";
-		// });
-var scene2 = new ScrollMagic.Scene({
-	triggerElement: '#scene-4'})
-		.setPin('#pinned-container');//pinner første brik så den flyder med videre ned når vi scroller
+	var animateElement = document.getElementById("midte"); //animation af farveændring
+	var myTween = new TweenMax.to("#brick2", 10.5, {x: '390'});
 
-var scene3 = new ScrollMagic.Scene({
-	triggerElement: '#scene-6'})
-		.setPin('#brick2')
-	// .on('enter', function(){
-	// 	$('#brick2').detach().appendTo('#pinned-container');
-	// });
+	//create a scene
+	var scene = new ScrollMagic.Scene({
+		triggerElement: '#scene-3'})//farven skifter når scene-3 rammer midten af viewport
+			.on('enter', function(){
+				animateElement.style.fill = 'grey';
+			});
+			// .on('leave', function(){
+			// 	animateElement.style.fill = "";
+			// });
+	var scene2 = new ScrollMagic.Scene({
+		triggerElement: '#scene-4'
+	}).setPin('#pinned-container-1');//pinner første brik så den flyder med videre ned når vi scroller
 
-controller.addScene([
-	scene,
-	scene2,
-	scene3
-]);
+	var scene3 = new ScrollMagic.Scene({
+		triggerElement: '#scene-6'
+	}).setPin('#pinned-container-2');
 
-var animateElement = document.getElementById("midte"); //animation af farveændring
+	var scene4 = new ScrollMagic.Scene({
+		triggerElement: '#scene-7', triggerhook:0, duration: 100
+	}).setTween(myTween);
+
+	controller.addScene([
+		scene,
+		scene2,
+		scene3,
+		scene4
+	]);
+
+});	
