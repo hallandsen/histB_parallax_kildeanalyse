4e5f60cced133a032fead30abc82e3e472b7afc0
hallandsen <jacob.e.halland@gmail.com>
2015-10-01 15:45:12 +0200

    iterativ commit

--- parallax_kildeanalyse.js
+++ parallax_kildeanalyse.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {
 // init controller
 	var controller = new ScrollMagic.Controller();
-
 	var animateElement = document.getElementById("midte"); //animation af farveændring
-	var myTween = new TweenMax.to("#brick2", 10.5, {x: '390'});
+
+	// var colorTween = new TweenMax.to('#midte', 1, {
+ //    	fill: 'grey',
+	// });
+	
+	var myTween = new TweenMax.to("#brick2", 0.5, {
+		x: '409'
+	});
 
 	//create a scene
-	var scene = new ScrollMagic.Scene({
-		triggerElement: '#scene-3'})//farven skifter når scene-3 rammer midten af viewport
+	var scene1 = new ScrollMagic.Scene({
+		triggerElement: '#scene-3', duration: 400})//farven skifter når scene-3 rammer midten af viewport
+			// .setTween('#midte',  {fill: "grey"});
+			// .setTween(colorTween);
 			.on('enter', function(){
 				animateElement.style.fill = 'grey';
 			});
-			// .on('leave', function(){
-			// 	animateElement.style.fill = "";
-			// });
+
 	var scene2 = new ScrollMagic.Scene({
 		triggerElement: '#scene-4'
 	}).setPin('#pinned-container-1');//pinner første brik så den flyder med videre ned når vi scroller
@@ -23,11 +29,11 @@ $(document).ready(function() {
 	}).setPin('#pinned-container-2');
 
 	var scene4 = new ScrollMagic.Scene({
-		triggerElement: '#scene-7', triggerhook:0, duration: 100
+		triggerElement: '#scene-7', triggerhook:0, duration: 800
 	}).setTween(myTween);
 
 	controller.addScene([
-		scene,
+		scene1,
 		scene2,
 		scene3,
 		scene4
