a119387ab954c92aa20b251a328c46335524170e
Jacob Erskov Halland <jaha@kvuc.dk>
2015-10-05 14:04:14 +0200

    Iterativ git

--- parallax_kildeanalyse.js
+++ parallax_kildeanalyse.js
@@ -7,36 +7,65 @@ $(document).ready(function() {
  //    	fill: 'grey',
 	// });
 	
-	var myTween = new TweenMax.to("#brick2", 0.5, {
-		x: '409'
+	var myTween = new TweenMax.to("#brick22", 0.5, {
+		x: '464'
 	});
 
 	//create a scene
+	// var scene1 = new ScrollMagic.Scene({
+	// 	triggerElement: '#scene-2'})//farven skifter når scene-3 rammer midten af viewport
+	// 	.on('enter', function(){
+	// 		animateElement.style.fill = 'grey';
+	// 	});
+
+	// var scene2 = new ScrollMagic.Scene({
+	// 	triggerElement: '#scene-2'
+	// }).setPin('#pinned-container-1');//pinner første brik så den flyder med videre ned når vi scroller
+
 	var scene1 = new ScrollMagic.Scene({
-		triggerElement: '#scene-3', duration: 400})//farven skifter når scene-3 rammer midten af viewport
-			// .setTween('#midte',  {fill: "grey"});
-			// .setTween(colorTween);
-			.on('enter', function(){
-				animateElement.style.fill = 'grey';
-			});
+		triggerElement: '#scene-2',
+		duration: $(window).height() + ($(window).height()-400)
+	}).setPin('#no-1');//pinner første brik så den flyder med videre ned når vi scroller
+
 
 	var scene2 = new ScrollMagic.Scene({
-		triggerElement: '#scene-4'
-	}).setPin('#pinned-container-1');//pinner første brik så den flyder med videre ned når vi scroller
+		triggerElement: '#scene-3',
+		duration: $(window).height()-400
+	}).setPin('#expl-container-1');
 
 	var scene3 = new ScrollMagic.Scene({
-		triggerElement: '#scene-6'
-	}).setPin('#pinned-container-2');
+		triggerElement: '#scene-4',
+		duration: $(window).height() - 200
+	}).setPin('#brick1');//pinner første brik så den flyder med videre ned når vi scroller
+
+	//section2
 
 	var scene4 = new ScrollMagic.Scene({
-		triggerElement: '#scene-7', triggerhook:0, duration: 800
+		triggerElement: '#scene-5',
+		duration: $(window).height() + ($(window).height()-400)
+	}).setPin('#no-2');
+
+	var scene5 = new ScrollMagic.Scene({
+		triggerElement: '#scene-6',
+		duration: $(window).height()-400
+	}).setPin('#expl-container-2');
+
+	var scene5 = new ScrollMagic.Scene({
+		triggerElement: '#scene-7',
+		duration: $(window).height()
+	}).setPin('#brick-container-no-2');
+
+	var scene6 = new ScrollMagic.Scene({
+		triggerElement: '#scene-7', duration: $(window).height()
 	}).setTween(myTween);
 
 	controller.addScene([
 		scene1,
 		scene2,
 		scene3,
-		scene4
+		scene4,
+		scene5,
+		scene6
 	]);
 
 });	
